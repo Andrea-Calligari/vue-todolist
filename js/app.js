01e418c9be506f38bcad9ea5a26cfcd40d555a9d
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-03-27 22:56:19 +0100

    ADD: bonus-1

--- js/app.js
+++ js/app.js
@@ -1,40 +1,45 @@
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-       toDoList:[
-        {
-            text: 'Andare dal barbiere',
-            done: false
+    data() {
+        return {
+
+            toDoList: [
+                {
+                    text: 'Spazzare il giardino',
+                    done: false
+                },
+                {
+                    text: 'Portare la macchina dal meccanico',
+                    done: false
+                },
+                {
+                    text: 'Andare dalla nonna',
+                    done: false
+                },
+                {
+                    text: 'Portare fuori il cane',
+                    done: false
+                },
+            ],
+            valueInput: '',
+            classLine: 'line-trought'
+
+        }
+    },
+    methods: {
+        removeTask(i) {
+            this.toDoList.splice(i, 1);
         },
-        {
-            text: 'Andare a fare la spesa',
-            done: true
-        },
-        {
-            text: 'Pulire la casa',
-            done: true
-        },
-        {
-            text: 'Spazzare il giardino',
-            done: false
-        },
-        {
-            text: 'Portare fuori il cane',
-            done: false
-        },
-        {
-            text: 'Andare dalla nonna',
-            done: true
-        },
-    ]
-    
-}
-},
-methods:{
-    removeTask(i){
-       this.toDoList.splice(i, 1);
+
+        addTask() {
+            if (this.valueInput.trim() !== '') {
+                this.toDoList.push({
+                    text: this.valueInput,
+                    done: false
+                })
+            }
+            this.valueInput = ''
+        }
     }
-}
 }).mount('#app')
